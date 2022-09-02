f5ae8797adefb76af9e1cdfc98b0f47de18ed44b
Jose A. Fernandez <fernandezja@gmail.com>
2022-09-02 19:43:58 -0300

    Simple Soldier object (with test)

---
+++ src/main/java/com/fernandezja/battle/Soldier.java
@@ -0,0 +1,16 @@
+package com.fernandezja.battle;
+
+
+public class Soldier 
+{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/test/java/com/fernandezja/battle/SoldierTest.java
@@ -0,0 +1,19 @@
+package com.fernandezja.battle;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class SoldierTest 
+{
+    @Test
+    public void soldier_simple_test()
+    {
+        Soldier soldier1 = new Soldier();
+        soldier1.setName("Soldier 1");
+
+        assertEquals("Soldier 1", soldier1.getName());
+    }
+}
