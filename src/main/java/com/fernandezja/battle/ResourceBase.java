d2b6ebab4957b1d9be3dcd37fcf6dfcbc8044511
Jose A. Fernandez <fernandezja@gmail.com>
2022-09-09 20:01:40 -0300

    ResourseBase (abstract class). Soldier extends from ResourseBase

---
+++ src/main/java/com/fernandezja/battle/ResourceBase.java
@@ -0,0 +1,28 @@
+package com.fernandezja.battle;
+
+import com.fernandezja.battle.interfaces.IPrintable;
+
+public abstract class ResourceBase 
+                        implements IPrintable
+{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String print() {
+       
+        return "generic print";
+    }
+
+    public String printWithOutTest() {
+       
+        return "printWithOutTest ";
+    }
+}
--- src/main/java/com/fernandezja/battle/Soldier.java
+++ src/main/java/com/fernandezja/battle/Soldier.java
@@ -1,29 +1,10 @@
 package com.fernandezja.battle;
 
-import com.fernandezja.battle.interfaces.IPrintable;
+
 
 public class Soldier 
-        implements IPrintable
+                extends ResourceBase
 {
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String print() {
-       
-        return "generic print";
-    }
-
-    public String printWithOutTest() {
-       
-        return "printWithOutTest ";
-    }
+   
 
 }
